6e7ffdb5af14057b77cff39abcd6db94b060b9d9
davidtnfsh <davidtnfsh@gmail.com>
2017-03-24 10:26:15 +0800

    情緒版的文章,還是以整篇文章當作一份情緒的資料，一句一句段有太多雜訊和情緒未定的句子出現

--- PTT/Negative/HatePolitics/main.py
+++ PTT/Negative/HatePolitics/main.py
@@ -7,4 +7,4 @@ content_file = open(topic+'.content.txt', 'w', encoding='utf-8')
 for i in json.load(open(sys.argv[1], 'r', encoding='utf-8'))['articles']:
 	if i.get('article_title', '')!=None and '[黑特]' in i.get('article_title', '') and 'R' not in i.get('article_title', ''):
 		title_file.write(i.get('article_title', '').replace('\n', '') + '\n')
-		f.write(i.get('content', '').replace('\n', '') + '\n')
+		content_file.write(i.get('content', '').replace('\n', '') + '\n')
--- PTT/Negative/sad/main.py
+++
@@ -1,6 +0,0 @@
-import json, sys
-with open(sys.argv[2], 'w', encoding='utf-8') as f:
-	for i in json.load(open(sys.argv[1], 'r', encoding='utf-8'))['articles']:
-		if i.get('article_title', '')!=None and '[公告]' not in i.get('article_title', '') :
-			f.write(i.get('article_title', '').replace('\n', '') + '\n')
-			list(map(lambda x:f.write(x + '\n'), i.get('content', '').replace('\n', '').split()))
