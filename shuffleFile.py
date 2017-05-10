09e07a7c9c8ef3bba66eb0877be7cfa8cd395f46
david <davidtnfsh@gmail.com>
2017-05-10 19:25:36 +0800

    [Bug fix]

--- shuffleFile.py
+++ shuffleFile.py
@@ -1,6 +1,6 @@
 from random import shuffle
 import sys
-result = list(map(lambda x:x, open(sys.argv[0], 'r', encoding='utf-8')))
+result = list(map(lambda x:x, open(sys.argv[1], 'r', encoding='utf-8')))
 shuffle(result)
 with open('pos.txt', 'w') as f:
         for i in result:
